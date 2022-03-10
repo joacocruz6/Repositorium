bc2d30d95e3b7d7f69f4c4e635211a118fd996bd
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-10 20:33:46 -0300

    feat(utils): create BaseUUIDModel

---
+++ Repositorium/utils/models.py
@@ -0,0 +1,11 @@
+import uuid
+from django.db import models
+
+
+class BaseUUIDModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
