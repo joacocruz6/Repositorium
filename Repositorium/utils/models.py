ab5ff31dfa5d6cd3f5cb732e6b8d0c7c27710b9d
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-10 21:35:58 -0300

    fix(utils): change utils default uuid

--- Repositorium/utils/models.py
+++ Repositorium/utils/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class BaseUUIDModel(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
