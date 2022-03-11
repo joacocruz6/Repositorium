7c9b04f182e7369f8ab993ec47f5cbe43157afdc
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-10 21:30:58 -0300

    feat(learning_resources): add categories model

--- Repositorium/learning_resources/models.py
+++ Repositorium/learning_resources/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from repositorium.utils.models import BaseUUIDModel
 
 # Create your models here.
+
+
+class Categories(BaseUUIDModel):
+    name = models.CharField(max_length=150)
