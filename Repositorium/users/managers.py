eb8d0fe94289a80aae340e76ddac0361843e915e
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-20 19:24:22 -0300

    feat(managers): create user manager

---
+++ Repositorium/users/managers.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+from typing import Union
+from repositorium.users.models import User
+from repositorium.users.exceptions import UserDoesNotExists
+
+
+def get_user_by_email(email: str) -> User:
+    user = User.objects.filter(email=email).first()
+    if user is None:
+        raise UserDoesNotExists
+    return user
+
+
+def create_user(email: str, first_name: str, last_name: str, password: str) -> User:
+    user = User.objects.create(email=email, first_name=first_name, last_name=last_name)
+    user.set_password(password)
+    user.save()
+    return user
