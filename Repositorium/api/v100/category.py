f723afa0246df25a55f193fb12c5a206bc5aca56
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-23 20:47:32 -0300

    feat(api): add category viewset

--- Repositorium/api/v100/category.py
+++ Repositorium/api/v100/category.py
@@ -6,6 +6,12 @@ from repositorium.api.serializers.category import (
     CategorySerializer,
     CategoryCreateSerializer,
 )
+from repositorium.learning_resources.managers import category as category_manager
+from repositorium.learning_resources.exceptions import (
+    CategoryAlreadyExists,
+    CategoryDoesNotExists,
+)
+from django.core.paginator import Paginator, EmptyPage
 
 
 class CategoryViewSet(ViewSet):
@@ -13,9 +19,48 @@ class CategoryViewSet(ViewSet):
         serializer = CategoryCreateSerializer(data=request.data)
         if serializer.is_valid():
             name = serializer.data["name"]
+            try:
+                category = category_manager.create_category(name=name)
+            except CategoryAlreadyExists:
+                data = {"errors": {"name": ["Category already exists."]}}
+                return Response(status=status.HTTP_400_BAD_REQUEST, data=data)
+            else:
+                category_serializer = CategorySerializer(instance=category)
+                return Response(
+                    status=status.HTTP_201_CREATED, data=category_serializer.data
+                )
+        else:
+            data = {"errors": serializer.errors}
+            return Response(status=status.HTTP_400_BAD_REQUEST, data=data)
 
-    def list(self, request: Request) -> Response:
-        pass
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        per_page = request.query_params.get("per_page", 10)
+        page_number = request.query_params.get("page_number", 1)
+        categories = category_manager.get_all_categories()
+        paginator = Paginator(categories, per_page)
+        try:
+            page = paginator.get_page(page_number)
+        except EmptyPage:
+            data = {
+                "page_number": page_number,
+                "has_next_page": False,
+                "categories": [],
+            }
+            return Response(status=status.HTTP_204_NO_CONTENT, data=data)
+        else:
+            serializer = CategorySerializer(page.object_list, many=True)
+            data = {
+                "page_number": page_number,
+                "has_next_page": page.has_next,
+                "categories": serializer.data,
+            }
+            return Response(status=status.HTTP_200_OK, data=data)
 
-    def retrieve(self, request: Request, pk: str = None) -> Response:
-        pass
+    def retrieve(self, request: Request, pk: str = None, *args, **kwargs) -> Response:
+        try:
+            category = category_manager.get_category(name=pk)
+        except CategoryDoesNotExists:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            serializer = CategorySerializer(category)
+            return Response(status=status.HTTP_200_OK, data=serializer.data)
