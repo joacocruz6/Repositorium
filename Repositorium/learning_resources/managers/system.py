c20365e744afd78ad05bd6bfb6987937ea2a9d7a
Joaquín Cruz <joacocruz6@gmail.com>
2022-03-24 22:27:16 -0300

    feat(managers): create system managers

--- Repositorium/learning_resources/managers/system.py
+++ Repositorium/learning_resources/managers/system.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+from typing import Tuple, Union
+from django.db.models import QuerySet
 from repositorium.learning_resources.models import System
 from repositorium.learning_resources.exceptions import (
     SystemAlreadyExists,
@@ -16,4 +19,22 @@ def create_system(name: str) -> System:
 
 
 def get_system_by_name(name: str) -> System:
-    pass
+    system = System.objects.filter(name=name).first()
+    if system is None:
+        raise SystemDoesNotExists
+    return system
+
+
+def get_system_by_uuid(uuid: Union[str, UUID]) -> System:
+    system = System.objects.filter(uuid=uuid).first()
+    if system is None:
+        raise SystemDoesNotExists
+    return system
+
+
+def get_or_create_system(name: str) -> Tuple[System, bool]:
+    return System.objects.get_or_create(name=name)
+
+
+def get_all_systems() -> QuerySet:
+    return System.objects.all()
