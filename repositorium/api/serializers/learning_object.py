04cdcfa9df72c72c2a076b3006c60256c62785bf
Joaquin Cruz <joacocruz6@gmail.com>
2022-04-10 18:22:44 -0400

    feat(learning_object.serializers): add new serializers for the learning objects

--- repositorium/api/serializers/learning_object.py
+++ repositorium/api/serializers/learning_object.py
@@ -4,10 +4,41 @@ from repositorium.api.serializers.base import BaseSerializer
 from repositorium.api.serializers.category import CategorySerializer
 
 
-class LearningObjectListSerializer(BaseSerializer):
-    name = serializers.CharField()
+class LearningObjectCreateSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    categories = serializers.ListField(child=serializers.CharField())
+    content = serializers.CharField()
+    system = serializers.CharField()
+    extra_data = serializers.JSONField()
+
+
+class LearningObjectSerializer(BaseSerializer):
+    title = serializers.CharField()
+    content = serializers.CharField()
     categories = CategorySerializer(many=True)
+    extra_data = serializers.JSONField()
+
+    # Property
+    is_forked = serializers.BooleanField()
+
+    # Method fields
     creator_name = serializers.SerializerMethodField()
+    system = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
 
     def get_creator_name(self, instance) -> str:
         return instance.created_by.full_name
+
+    def get_system(self, instance) -> str:
+        return instance.created_on.name
+
+    def get_average_rating(self, instance) -> float:
+        return None
+
+
+class LearningObjectForkSerializer(serializers.Serializer):
+    system_uuid = serializers.UUIDField()
+
+
+class LearningObjectRateSerializer(serializers.Serializer):
+    rating = serializers.DecimalField()
