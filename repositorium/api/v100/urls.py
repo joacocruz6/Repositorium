21d559246b3aa5a7ef0f5fe9c808f716a6896a6a
Joaquin Cruz <joacocruz6@gmail.com>
2022-06-16 19:26:03 -0400

    feat(files): add files management views to the urls

--- repositorium/api/v100/urls.py
+++ repositorium/api/v100/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from repositorium.api.v100.category import CategoryViewSet
 from repositorium.api.v100.learning_objects import LearningObjectViewSet
+from repositorium.api.v100.learning_objects_files import FileManagementAPIView
 from repositorium.api.v100.recomender import RecomenderViewSet
 from repositorium.api.v100.systems import SystemViewSet
 from repositorium.api.v100.users import AuthViewSet, UserViewSet
@@ -15,6 +16,12 @@ router.register(r"learning_object", LearningObjectViewSet, basename="learning_ob
 router.register(r"auth", AuthViewSet, basename="authentication")
 router.register(r"recomend", RecomenderViewSet, basename="recomender")
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "learning_object/files/<uuid:uuid>/",
+        FileManagementAPIView.as_view(),
+        name="file_management_view",
+    ),
+]
 
 urlpatterns += router.urls
