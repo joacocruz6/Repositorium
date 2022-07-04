4e62a03f45563d60ac01ae7450ab7eb7ddb17307
Joaquin Cruz <joacocruz6@gmail.com>
2022-07-04 18:18:22 -0400

    fix(urls): fix typo in urls

--- repositorium/api/urls.py
+++ repositorium/api/urls.py
@@ -4,6 +4,6 @@ from repositorium.api.views import healthz
 
 app_name = "api"
 urlpatterns = [
-    path("healtz/", healthz, name="health"),
+    path("healthz/", healthz, name="health"),
     path("v100/", include("repositorium.api.v100.urls")),
 ]
